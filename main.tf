c47eb6f708e7c08f70816b82f2c6b8a77193c425
Jacob Martinson <jacobm3@gmail.com>
2019-05-12 15:24:21 -0500

    Update resources.

--- main.tf
+++ main.tf
@@ -2,8 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
-  ami          = "ami-2757f631"
-  instance_type = "t2.micro"
-  count        = 1
+# resource "aws_instance" "example" {
+#   ami          = "ami-2757f631"
+#   instance_type = "t2.micro"
+#   count        = 1
+# }
+
+resource "aws_s3_bucket" "b" {
+  bucket = "jmartinson-d41d8cd98f00b204e9800998ecf8427e"
+  acl    = "private"
+
+  tags = {
+    owner = "jmartinson@hashicorp.com"
+  }
 }
+
